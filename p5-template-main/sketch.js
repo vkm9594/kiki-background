4195d7f362cb8894178e08c32e371e5e17ba74cc
vkm9594 <vkm9594@github.com>
2020-10-27 14:10:02 -0400

    New background and building

--- p5-template-main/sketch.js
+++ p5-template-main/sketch.js
@@ -1,69 +1,48 @@
-let movingClouds;
+let scenery;
+let rect = [];
 let buildings = [];
 
 function preload() {
-  movingClouds = loadImage('floating-clouds.gif');
-  for(i = 0; i < 6; i++) {
-    buildings[i] = loadImage('buildings/building'+ i + '.png');
+  scenery = loadImage('buildings/background.jpg');
+  for (i = 0; i < 6; i++) {
+    buildings[i] = loadImage('buildings/building' + i + '.png');
   }
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-}
-
-function draw() {
-  var color1 = color(2, 144, 212);
-  var color2 = color(222, 244, 255);
-  setGradient(0, 0, windowWidth, windowHeight, color1, color2, "Y");
-
-  cloudsInSky();
-  build();
-
-}
- 
-
-function setGradient(x, y, w, h, c1, c2, axis) {
-  noFill();
-  if (axis == "Y") { // Top to bottom gradient
-    for (let i = y; i <= y + h; i++) {
-      var inter = map(i, y, y + h, 0, 1);
-      var c = lerpColor(c1, c2, inter);
-      stroke(c);
-      line(x, i, x + w, i);
-    }
-  } else if (axis == "X") { // Left to right gradient
-    for (let j = x; j <= x + w; j++) {
-      var inter2 = map(j, x, x + w, 0, 1);
-      var d = lerpColor(c1, c2, inter2);
-      stroke(d);
-      line(j, y, j, y + h);
-    }
+  for (let i = 0; i < 1000; i++) {
+    let x = width;
+    let y = height - 430;
+    let w = random(50, 150);
+    let h = random(200, 500);
+    let building = random(buildings);
+    let b = new AddBuilding(x, y, w, h, building);
+    rect.push(b);
   }
 }
 
-function cloudsInSky() {
-  push();
-  imageMode(CORNER);
-  // scale(0.3);
-  image(movingClouds, 0, 0, width, movingClouds.height*width/movingClouds.width);
-  pop();
+function draw() {
+  image(scenery, 0, 0, width, height)
+
+  rect[i].update();
+  rect[i].draw();
 }
 
 class AddBuilding {
-  constructor(x, y, r, img) {
+  constructor(x, y, w, h) {
     this.x = x;
     this.y = y;
-    this.r = r;
+    this.w = w;
+    this.h = h;
     this.building = random(buildings);
   }
 
   update() {
-    //translate(-frameCount * 3, 0);
+    translate(-frameCount * 3.5, 0);
   }
 
   draw() {
-   
-  image(buildings[1], 0, 0);
+    image(this.building, this.x, this.y, this.r);
   }
 }
